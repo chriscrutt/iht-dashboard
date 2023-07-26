887fd8b547972a9a8bd84226cd68a5dc2eef7605
Chris <chriscrutt@gmail.com>
2023-07-25 23:58:46 -0700

    separated files

---
+++ script.js
@@ -0,0 +1,60 @@
+function switchTab(tabId, mainTabId, secondaryTabId) {
+    var tabs = document.getElementsByClassName("content");
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].classList.add("hidden");
+    }
+
+    var mainTabs = document.getElementsByClassName("navbar")[0].getElementsByTagName("a");
+    for (var i = 0; i < mainTabs.length; i++) {
+        mainTabs[i].classList.remove("selected");
+    }
+    document.getElementById(mainTabId).classList.add("selected");
+
+    var secondaryTabs = document.getElementsByClassName("secondary-navbar");
+    for (var i = 0; i < secondaryTabs.length; i++) {
+        secondaryTabs[i].classList.add("hidden");
+    }
+    document.getElementById(secondaryTabId).classList.remove("hidden");
+
+    var secondaryTabLinks = document.querySelectorAll(".secondary-navbar a");
+    for (var i = 0; i < secondaryTabLinks.length; i++) {
+        secondaryTabLinks[i].classList.remove("selected");
+    }
+
+    return false;
+}
+
+function switchContent(contentId, tabId) {
+    var contents = document.getElementsByClassName("content");
+    for (var i = 0; i < contents.length; i++) {
+        contents[i].classList.add("hidden");
+    }
+    document.getElementById(contentId).classList.remove("hidden");
+
+    var secondaryTabs = document.querySelectorAll(".secondary-navbar a");
+    for (var i = 0; i < secondaryTabs.length; i++) {
+        secondaryTabs[i].classList.remove("selected");
+    }
+    document.getElementById(tabId).classList.add("selected");
+
+    return false;
+}
+
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ["Task", "Hours per Day"],
+        ["Work", 11],
+        ["Eat", 2],
+        ["Commute", 2],
+        ["Watch TV", 2],
+        ["Sleep", 7],
+    ]);
+
+    var options = {
+        title: "My Daily Activities",
+        pieHole: 0.4,
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById("chart_div"));
+    chart.draw(data, options);
+}
